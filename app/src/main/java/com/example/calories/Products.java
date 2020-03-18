b5f370233a015822ba041f34bee571f0c7ca67fd
Olya Sergienko <OLga.250299@yandex.ru>
2020-03-18 11:49:45 +0300

    (FireBase) RecyclerView instead ListView first

--- app/src/main/java/com/example/calories/Products.java
+++ app/src/main/java/com/example/calories/Products.java
@@ -1,48 +1,15 @@
 package com.example.calories;
 
 public class Products {
-    public String белки, жиры, углеводы;
-    public Long калорийность;
+    public String белки, жиры, углеводы, калорийность;
 
     public Products() {
     }
 
-    public Products(String белки, String жиры, String углеводы, Long калорийность) {
+    public Products(String белки, String жиры, String углеводы, String калорийность) {
         this.белки = белки;
         this.жиры = жиры;
         this.углеводы = углеводы;
-        this.калорийность = калорийность;
-    }
-
-    public String getБелки() {
-        return белки;
-    }
-
-    public void setБелки(String белки) {
-        this.белки = белки;
-    }
-
-    public String getЖиры() {
-        return жиры;
-    }
-
-    public void setЖиры(String жиры) {
-        this.жиры = жиры;
-    }
-
-    public String getУглеводы() {
-        return углеводы;
-    }
-
-    public void setУглеводы(String углеводы) {
-        this.углеводы = углеводы;
-    }
-
-    public Long getКалорийность() {
-        return калорийность;
-    }
-
-    public void setКалорийность(Long калорийность) {
         this.калорийность = калорийность;
     }
 }
