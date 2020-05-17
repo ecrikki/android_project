48f124f52b81cd56707d74655fef91102f57a894
Marinasidorova <sidorovamarin@gmail.com>
2020-05-17 18:04:37 +0300

    parameter activity

--- app/src/main/java/com/example/calories/Parameter_Activity.java
+++ app/src/main/java/com/example/calories/Parameter_Activity.java
@@ -1,15 +1,21 @@
 package com.example.calories;
 
-import android.app.Activity;
+
 import android.os.Bundle;
-
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Parameter_Activity extends Activity {
+
+public class Parameter_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parameter_activity);
+
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.logo_small);
     }
 }
