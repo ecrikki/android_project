7fedffdccb128dcc5c8f026dec1969da6fa98597
Marinasidorova <sidorovamarin@gmail.com>
2020-05-24 23:26:15 +0300

    settings_activity works

--- app/src/main/java/com/example/calories/Parameter_Activity.java
+++ app/src/main/java/com/example/calories/Parameter_Activity.java
@@ -4,6 +4,11 @@ package com.example.calories;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -49,5 +54,70 @@ public class Parameter_Activity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setLogo(R.drawable.logo_small);
+    }
+
+    public void onClick_Calc(View view) {
+        TextView tv = (TextView)findViewById(R.id.tvResult);
+
+        EditText ves = (EditText)findViewById(R.id.editText);
+        int ves_int = Integer.parseInt(ves.getText().toString());
+        EditText rost = (EditText)findViewById(R.id.editText2);
+        int rost_int = Integer.parseInt(rost.getText().toString());
+        EditText vozrast = (EditText)findViewById(R.id.editText3);
+        int vozrast_int = Integer.parseInt(vozrast.getText().toString());
+
+        RadioButton mm = (RadioButton)findViewById(R.id.radio_m);
+//        RadioButton ww = (RadioButton)findViewById(R.id.radio_w);
+
+
+        RadioGroup rgActiv = (RadioGroup)findViewById(R.id.radioGroup2);
+        int radioButtonID = rgActiv.getCheckedRadioButtonId();
+
+        View radioButton = rgActiv.findViewById(radioButtonID);
+        int activnost = rgActiv.indexOfChild(radioButton);
+        double BMR = 0;
+        double level = 0;
+        double answer = 0;
+        boolean man;
+        if (mm.isChecked())
+        {
+            // man
+            man=true;
+            BMR = 88.36 + (13.4 * ves_int) + (4.8 * rost_int) - (5.7 * vozrast_int);
+        } else
+            {
+                // women
+            man=false;
+            BMR = 447.6 + (9.2 * ves_int) + (3.1 * rost_int) - (4.3 * vozrast_int);
+        }
+
+        if (activnost == 0)
+        {
+            level = 1.2;
+        }
+        else if (activnost == 1)
+        {
+            level = 1.375;
+        }
+        else if (activnost == 2)
+        {
+            level = 1.55;
+        }
+        else if (activnost == 3)
+        {
+            level = 1.725;
+        }
+        else
+        {
+            level = 1.9;
+        }
+//        rgActiv.getCheckedRadioButtonId();
+        //String s = (man)?"men":"women";
+        //s = s + " "+ (new Integer(activnost)).toString();
+        answer = BMR * level;
+        String s = "";
+        s = (new Integer((int)Math.round(answer))).toString();
+        tv.setText(s);
+
     }
 }
